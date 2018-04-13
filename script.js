96def577f37ba0acdccddca8078726234d593be7
Greg Orvik <gvorvik@gmail.com>
2018-04-13 11:09:55 -0500

    Add starting files

---
+++ script.js
@@ -0,0 +1,16 @@
+//options for github include 
+// 1. creating a repo(folder) with a ReadMe on github and git cloning it down
+    //github will make the initial commit for you as long as a file(ReadMe) is created
+    //once changes are made, git add, git commit(with message) and git push
+// 2. Create folder on computer (mkdir) and push up to github
+    //cd in to the folder you just created
+    //run git init to create a git repo(.git) 
+    //run git add and git commit
+    //run git remote add origin(new name for the url of the repo) <name of place where the repo is going>
+    //create an EMPTY (no ReadMe) folder on github where the folder will be pushed to
+
+console.log('In script.js');
+
+$(document).ready(function() {
+    console.log('Jquery is working');
+});
